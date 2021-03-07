57c28edfa3ac0e188ab67f351694623977075cfc
Damilola <dami_fagbo@yahoo.com>
2021-03-07 22:32:32 +0100

    updates

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 const buttonClick = document.querySelector('.btn');
-const contain = document.querySelector('.container');
 
-//styling
 
-contain.style.textAlign = 'center';
 
+//Add event listener
 buttonClick.addEventListener('click', clickBtn);
 
 function clickBtn(e){
-    let color = ["#03396c","purple","#b3cde0","yellow","red",'black',"orange",'#9ca0a0'];
+    let color = ["#bfb5b2","#f7347a","#03396c","purple", "brown","#b3cde0","yellow","red",'black',"orange",'#9ca0a0'];
     let randomColor = color[Math.floor(Math.random() * color.length)];
         document.body.style.background = randomColor;
-    
+        
     
 }
